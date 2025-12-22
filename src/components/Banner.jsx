cb693f9133a7cf43ca20ab336f18a17c39504f83
Gabriel Marius Popescu <gabrielmariuspopescu@hotmail.com>
2025-12-22 19:56:10 +0000

    initial commit on globomantics

---
+++ src/components/Banner.jsx
@@ -0,0 +1,22 @@
+import logo from '../assets/globomantics.png';
+import styles from './Banner.module.css';   
+
+const subtitleStyle = {
+    fontStyle: "italic",
+    fontSize: "x-large",
+    color: "coral"
+};
+
+const Banner = () => {
+    return (
+        <header className="row">
+            <div className="col-5 mb-4">
+                <img src={logo} className={styles.logo} alt="logo" />
+            </div>
+            <div className="col-7 mt-5" style={subtitleStyle}>
+                Providing houses all over the world.</div>
+        </header>
+    );
+}
+
+export default Banner;
